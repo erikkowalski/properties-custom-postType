34a7181e1077746a1c0d141b957d8a19a0f568f7
erikkowalski <erikkowalski@mac.com>
2015-06-09 14:47:02 -0400

    inital commit

---
+++ properties-cpt.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: Properties Custom Post Type
+*/
+
+
+add_action( 'init', 'create_post_type' );
+//Registers the Product's post type
+function create_post_type() {
+    register_post_type( 'psh_properties',
+                       array(
+                           'labels' => array(
+                               'name' => __( 'Properties' ),
+                               'singular_name' => __( 'Properties' )
+                           ),
+                           'public' => true,
+                           'has_archive' => true,
+                           'rewrite' => array('slug' => 'properties'),
+                       )
+                      );
+}
+
+?>
+
