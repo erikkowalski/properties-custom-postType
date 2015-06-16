95f34675a06918dd1371e84f07ad36d16d70d2c5
erikkowalski <erikkowalski@mac.com>
2015-06-16 14:49:17 -0400

    Added News custom post type

--- properties-cpt.php
+++ properties-cpt.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name: Properties Custom Post Type
+ * Plugin Name: PSH Custom Post Types
  * Plugin URI: http://edkstudio.com
  * Description: A simple plugin that properties post types and taxonomies
  * Version: 0.1
@@ -67,11 +67,54 @@ function create_post_type() {
     register_post_type( 'psh_properties', $args );
 }
 
+
 add_action( 'init', 'create_post_type' );
+
+//Registers the Product's post type
+function create_news_post_type() {
+    $labels = array(
+        'name'               => 'News',
+        'singular_name'      => 'News Story',
+        'menu_name'          => 'News',
+        'name_admin_bar'     => 'News Story',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New News Story',
+        'new_item'           => 'New News Story',
+        'edit_item'          => 'Edit News Story',
+        'view_item'          => 'View News Story',
+        'all_items'          => 'All News',
+        'search_items'       => 'Search News',
+        'parent_item_colon'  => 'Parent News:',
+        'not_found'          => 'No News found.',
+        'not_found_in_trash' => 'No News found in Trash.',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'menu_icon'          => 'dashicons-rss',
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'news' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
+        'taxonomies'         => array( 'category', 'post_tag' )
+    );
+
+    register_post_type( 'psh_news', $args );
+}
+
+add_action( 'init', 'create_news_post_type' );
 
 // Flush rewrite rules to add "review" as a permalink slug
 function my_rewrite_flush() {
     create_post_type();
+    create_news_post_type();
     flush_rewrite_rules();
 }
 register_activation_hook( __FILE__, 'my_rewrite_flush' );
