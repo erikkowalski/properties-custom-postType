3b29e3636a9e49fbf047804c0131cb11a7569dfd
erikkowalski <erikkowalski@mac.com>
2015-06-23 11:20:20 -0400

    initial commit

---
+++ includes/reuters-financial-feed.php
@@ -0,0 +1,43 @@
+<?php
+//Registers the Product's post type
+function create_financial_post_type() {
+	$labels = array(
+		'name'               => 'Financial News',
+		'singular_name'      => 'News Story',
+		'menu_name'          => 'Financial News',
+		'name_admin_bar'     => 'News Story',
+		'add_new'            => 'Add New',
+		'add_new_item'       => 'Add New News Story',
+		'new_item'           => 'New News Story',
+		'edit_item'          => 'Edit News Story',
+		'view_item'          => 'View News Story',
+		'all_items'          => 'All News',
+		'search_items'       => 'Search News',
+		'parent_item_colon'  => 'Parent News:',
+		'not_found'          => 'No News found.',
+		'not_found_in_trash' => 'No News found in Trash.',
+	);
+
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'menu_icon'          => 'dashicons-rss',
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'news' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
+		'taxonomies'         => array( 'category', 'post_tag' )
+	);
+
+	register_post_type( 'financial', $args );
+}
+
+add_action( 'init', 'create_financial_post_type' );
+
+?>
